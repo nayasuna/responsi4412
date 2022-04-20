5df9b349ce343eddd2d55042cea2495423925c0f
nayasuna <nayasuna01@students.amikom.ac.id>
2022-04-20 09:29:02 +0700

    inisialisasi project

---
+++ karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4412
+{
+    class Karyawan
+    {
+        // constructor default
+        public Karyawan()
+        {
+
+        }
+        // overload constructor 
+        public Karyawan(string nik, string nama, int gajiperbulan)
+        {
+            NIK = nik;
+            NAMA = nama;
+            Gajiperbulan = gajiperbulan;
+        }
+
+        // properties
+        public string NAMA { get; set; }
+        public string NIK { get; set; }
+        public int Gajiperbulan { get; set; }
+    }
+}
